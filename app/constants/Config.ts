97454e36edbd7393f6ec27c6e2bc1e7b125d9a6e
Rajan Giri <rajan.giri225@gmail.com>
2026-08-17 11:33:20 +0530

    Update app , frontend and Backend set final test

--- app/constants/Config.ts
+++ app/constants/Config.ts
@@ -1,11 +1,9 @@
 // Replace with your actual production URL
-// const PROD_API_URL = 'https://backend.travellersdeal.com/api';
-const ip = '10.148.155.73';
-const PROD_API_URL = `http://${ip}:5001/api`;
+const PROD_API_URL = 'https://backend.travellersdeal.com/api';
+
 // Connect to local backend for development (must use local IP for Expo/mobile to reach host PC)
 // However, User requested to fetch from the live PRODUCTION server directly instead of local
-// const DEV_API_URL = 'https://backend.travellersdeal.com/api';
-const DEV_API_URL = `http://${ip}:5001/api`;
+const DEV_API_URL = 'https://backend.travellersdeal.com/api';
 
 
 export const API_URL = __DEV__ ? DEV_API_URL : PROD_API_URL;
